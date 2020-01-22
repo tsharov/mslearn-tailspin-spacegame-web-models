b71f8b31fa2e75a9fbcbd1c8ef0a11b984e120cf
Taras Sharov <tsharov@yahoo.com>
2020-01-22 22:01:37 +0200

    Test 1

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -23,5 +23,7 @@ namespace TailSpin.SpaceGame.Web.Models
         // The game style (difficulty) the score is associated with.
         [JsonProperty(PropertyName = "gameStyle")]
         public string GameStyle { get; set; }
+
+        public string Field1 { get; set; }
     }
 }
